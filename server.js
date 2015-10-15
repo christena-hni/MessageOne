fc3157bc924a7811728bfbec6cf88ee00aad0499
christena-hni <christena@hnicorp.com>
2015-10-15 15:30:33 -0500

    Another Change

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
